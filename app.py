5eff45da6fc238039d54d65ea5ec0dfa18783a8f
abdelino17 <fare.abdel@gmail.com>
2021-04-23 02:31:57 +0200

    Initial Commit

--- app.py
+++ app.py
@@ -1,34 +1,15 @@
 #!/usr/bin/env python3
 import os
-
 from aws_cdk import core as cdk
+from cdk_lambda_dynamodb_fargate.cdk_lambda_dynamodb_stack import CdkLambdaDynamodbStack
+from cdk_lambda_dynamodb_fargate.cdk_fargate_stack import FargateStack
 
-# For consistency with TypeScript code, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
+app_env = {
+    "region": "us-east-1"
+}
+app = cdk.App()
 
-from cdk_lambda_dynamodb_fargate.cdk_lambda_dynamodb_fargate_stack import CdkLambdaDynamodbFargateStack
-
-
-app = core.App()
-CdkLambdaDynamodbFargateStack(app, "CdkLambdaDynamodbFargateStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=core.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=core.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+CdkLambdaDynamodbStack(app, "cdk-lambda-dynamodb", env=app_env)
+FargateStack(app, "cdk-fargate", env=app_env)
 
 app.synth()
